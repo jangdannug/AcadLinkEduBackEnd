36131fde73031e7f4cdd8da1365729fa7890f5f0
princess <prinxessannehingcocanazares@gmail.com>
2026-03-18 19:06:00 +0800

    added notif and its feature

---
+++ AcadLinkEduBackEnd.Domain/DTO/NotificationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcadLinkEduBackEnd.Domain.DTO
+{
+    public class NotificationDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
--- AcadLinkEduBackEnd.Domain/Notification.cs
+++ AcadLinkEduBackEnd.Domain/Notification.cs
@@ -8,11 +8,18 @@ namespace AcadLinkEduBackEnd.Domain.Entities
     {
         [PrimaryKey("id")]
         public int Id { get; set; }
+        [Column("user_id")]
         public int UserId { get; set; }
+        [Column("title")]
         public string Title { get; set; } = null!;
+        [Column("message")]
         public string Message { get; set; } = null!;
+        [Column("type")]
         public string Type { get; set; } = null!;
-        public DateTime CreatedAt { get; set; }
+        [Column("is_read")]
         public bool IsRead { get; set; }
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+      
     }
 }
